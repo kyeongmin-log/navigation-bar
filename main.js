ddfdfd1910605fc2dca6faf8e6e37d3b0c7619f0
kyeongmin-log <dotcom7860@gmail.com>
2021-03-24 21:50:04 +0900

    강의 보고 다시 만든 nav-bar. (완성도 부족 -> 강의 한번 더 보기)

---
+++ main.js
@@ -0,0 +1,7 @@
+var togglebtn = document.querySelector('nav__togglebtn');
+var menu = document.querySelector('.nav__menu');
+var icon = document.querySelector('.nav__icon');
+
+togglebtn.addEventListener('.active');
+menu.addEventListener('.active');
+icon.addEventListener('.active');
